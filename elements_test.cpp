cddb1c413e8432d3a2e6a0772a196aac141543cc
Antoine Mercier-Linteau <antoine@bitsofmymind.com>
2010-11-02 20:00:56 +0000

    Echo testing is working

--- elements_test.cpp
+++ elements_test.cpp
@@ -17,27 +17,61 @@
 void processing_wake(){}
 void processing_sleep(Elements::e_time_t time){}
 
+#include <stdlib.h>
+#include "string.h"
+
+class Echo: public Resource
+{
+	public:
+		bool echoed;
+
+	Echo(): Resource(), echoed(false)
+	{}
+
+	virtual Message* process(Response* response)
+	{
+		Resource::process(response);
+		echoed = true;
+
+		return NULL;
+	}
+
+};
+
 int main()
 {
-	//Resource* res1 = new Resource();
-	//Resource* res2 = new Resource();
-	//Resource* res3 = new Resource();
-	//Authority* root = new Authority();
-	Processing* proc = new Processing();
-	/*root->add_child(Elements::string<uint8_t>::make("proc"), proc);
-	root->add_child(Elements::string<uint8_t>::make("res1"), res1);
-	root->add_child(Elements::string<uint8_t>::make("res2"), res2);
-	res2->add_child(Elements::string<uint8_t>::make("res3"), res3);*/
+	Echo* echo = new Echo();
+	Resource* res2 = new Resource();
+	Resource* res3 = new Resource();
+	Authority* root = new Authority();
+	Processing* proc = new Processing(NULL);
 
-	const char* msg = "GET /res1/res3 HTTP/1.1\r\n\
+	root->add_child(Elements::string<uint8_t>::make("proc"), proc);
+	root->add_child(Elements::string<uint8_t>::make("echo"), echo);
+	root->add_child(Elements::string<uint8_t>::make("res2"), res2);
+	res2->add_child(Elements::string<uint8_t>::make("res3"), res3);
+
+	const char* cmsg = "GET /res2/res3 HTTP/1.1\r\n\
 Host: www.example.com\r\n\r\n";
+	char * msg = (char*)malloc(sizeof("GET /res2/res3 HTTP/1.1\r\n\
+Host: www.example.com\r\n\r\n"));
+	strcpy(msg,cmsg);
 	string<uint32_t> str = Elements::string<uint32_t>::make(msg);
 	Request* req = new Request();
+
 	req->deserialize(str, str.text);
 
-	//res2->send(req);
+	echo->send(req);
 
-	proc->start();
+	int steps = 0;
+
+	while(!echo->echoed)
+	{
+		proc->step();
+		steps++;
+	}
+
+	std::cout << "Echo completed in " << steps << " steps." << std::endl;
 
 	return 0;
 }
