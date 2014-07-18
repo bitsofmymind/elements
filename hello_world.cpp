be3b3524746f876b423c2fb3c9b7030b0796df4c
Antoine Mercier-Linteau <antoine@bitsofmymind.com>
2014-07-18 02:58:58 +0000

    Done commenting.

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,21 +1,36 @@
-/*
- * blink.cpp
+/* hello_world.cpp - a basic hello_world resource.
+ * Copyright (C) 2014 Antoine Mercier-Linteau
  *
- *  Created on: 2010-11-05
- *      Author: antoine
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+// INCLUDES
 
 #include "hello_world.h"
 #include <pal/pal.h>
 #include <stdlib.h>
 #include <iostream>
 #include <utils/template.h>
+#include <utils/memfile.h>
+#include <string.h>
 
 HelloWorld::HelloWorld():
 	Resource(),
 	state(true)
 {
-	const char* m = "Default message";
+	// Allocates block containing the default message.
+	const char* m = "Hello world!";
 	message = (char*)malloc(strlen(m) + 1);
 	strcpy(message, m);
 
@@ -29,74 +44,43 @@ HelloWorld::~HelloWorld()
 
 void HelloWorld::run(void)
 {
-	if(state)
+	if(state) // If the message should be displayed.
 	{
 		std::cout << message << std::endl;
 	}
-	schedule(2000);
+
+	schedule(SECONDS(2)); // Display message again in two seconds.
 }
 
-
-File<MESSAGE_SIZE>* HelloWorld::render( Request* request )
+Response::status_code HelloWorld::process( Request* request, Response* response )
 {
-	const char* r = \
-			"<html>\
-				<body>\
-					<h2>Hello World resource</h2>\
-					You can use the following form to change the message the resource is displaying in stdout and</br>\
-					turn it on or off.</br>\
-					<br/>\
-					<form method=\"post\" accept-charset=\"us-ascii\">\
-						Message: <input type=\"text\" id=\"msg\" name=\"msg\" value=\"~\"/><br/>\
-						ON<input type=\"radio\" id =\"st_1\" name=\"st\" ~ value=\"1\"/><br/>\
-						OFF<input type=\"radio\" id=\"st_0\" name=\"st\" ~ value=\"0\"/><br/>\
-						<input type=\"submit\" value=\"Submit\" />\
-					</form>\
-				</body>\
-			</html>";
+	// Let the parent class process the message.
+	Response::status_code sc = Resource::process(request, response);
 
-	//const char* r = "<html><body> ~ blah ~~ ~ </body></html>";
-
-	ConstFile<MESSAGE_SIZE>* f = new ConstFile<MESSAGE_SIZE>(r);
-
-	MESSAGE_SIZE data_len = strlen(message) + 1 + strlen("checked=\"checked\"") + 1 + 1;
-	char* data = (char*)ts_malloc(data_len);
-	char* ptr = data + strlen(message) + 1;
-	memcpy(data, message, strlen(message) + 1 );
-	if(state)
-	{
-		memcpy(ptr, "checked=\"checked\"", strlen("checked=\"checked\"") + 1);
-		ptr += strlen("checked=\"checked\"") + 1;
-		*ptr = '\0';
-	}
-	else
-	{
-		*ptr++ = '\0';;
-		memcpy(ptr, "checked=\"checked\"", strlen("checked=\"checked\"") + 1);
-	}
-
-	return new Template<MESSAGE_SIZE>(f, data, data_len, 3);
-}
-
-Response::status_code HelloWorld::process( Request* request, Message** return_message )
-{
-	Response::status_code sc = Resource::process(request, return_message);
+	 // If the parent class could not handle the message.
 	if(sc == NOT_IMPLEMENTED_501)
 	{
-		if(request->methodcmp("post", 4))
+		if(request->is_method("post")) // If this is a post request.
 		{
-			char buffer[50];
-			uint8_t len = request->find_arg("msg", buffer, 50);
+			char buffer[50]; // Holds the message.
 
-			if(len)
+			// Read the message argument from the request.
+			uint8_t len = request->find_arg("m", buffer, 50);
+
+			if(len) // If an argument containing the new message was found.
 			{
+				// Replace the current message with the new message.
 				free(message);
 				message = (char*)malloc(len + 1);
-				memcpy(message, buffer, len + 1);
+				strcpy(message, buffer);
 			}
+
+			// Read the state argument from the message.
 			len = request->find_arg("st", buffer, 1);
-			if(len)
+
+			if(len) // If there is a state argument in the request.
 			{
+				// Turns the displaying of the message on or off.
 				if(buffer[0] == '1')
 				{
 					state = true;
@@ -106,12 +90,69 @@ Response::status_code HelloWorld::process( Request* request, Message** return_me
 					state = false;
 				}
 			}
-
-			*return_message = http_get( request );
 		}
+		else if(!request->is_method("get")) // If the request is not a get.
+		{
+			return NOT_IMPLEMENTED_501;
+		}
+
+		const char* html = \
+		"<html>\
+			<body>\
+				<h2>Hello World resource</h2>\
+				You can use the following form to change the message the resource is displaying in stdout and</br>\
+				turn it on or off.</br>\
+				<br/>\
+				<form method=\"post\" accept-charset=\"us-ascii\">\
+					Message: <input type=\"text\" id=\"m\" name=\"m\" value=\"~\"/><br/>\
+					ON<input type=\"radio\" id =\"st_1\" name=\"st\" ~ value=\"1\"/><br/>\
+					OFF<input type=\"radio\" id=\"st_0\" name=\"st\" ~ value=\"0\"/><br/>\
+					<input type=\"submit\" value=\"Submit\" />\
+				</form>\
+			</body>\
+		</html>";
+
+		 // Creates a file to wrap the constant string above.
+		File* f = new MemFile((char*)html, true);
+
+		if(!f) // If the file could not be created.
+		{
+			return INTERNAL_SERVER_ERROR_500;
+		}
+
+		Template* t = new Template(f); // Create a template to hold the file.
+
+		if(!t) // If the template could not be created.
+		{
+			delete f; // Delete the allocated file.
+			return INTERNAL_SERVER_ERROR_500;
+		}
+
+		// Create a checked string for the input tag.
+		char* c = (char*)malloc(strlen("checked=\"checked\"") + 1);
+		strcpy(c, "checked=\"checked\"");
+
+		// Adds an argument for the message.
+		char* msg = (char*)malloc(strlen(message) + 1);
+		strcpy(msg, message);
+		t->add_arg(msg);
+
+		// Add the check argument to the correct input depending on the state.
+		if(!state)
+		{
+			t->skip_argument();
+		}
+		t->add_arg(c);
+		if(state)
+		{
+			t->skip_argument();
+		}
+
+		// Sets the body of the response.
+		response->set_body(t, MIME::TEXT_HTML);
+
+		return OK_200; // Done.
 	}
 
-	return sc;
+	return sc; // If the parent resource processed the request.
 }
-
-
--- hello_world.h
+++ hello_world.h
@@ -1,27 +1,68 @@
-/*
- * blink.h
+/* hello_world.h - a basic hello_world resource.
+ * Copyright (C) 2014 Antoine Mercier-Linteau
  *
- *  Created on: 2010-11-05
- *      Author: antoine
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 #ifndef HELLO_WORLD_H_
 #define HELLO_WORLD_H_
 
 #include <core/resource.h>
 
+/**
+ * A basic demonstration resource that displays a message and a simple form
+ * to modify the interval at which it prints a message to the output.
+ * */
 class HelloWorld: public Resource
 {
-	char* message;
-	bool state;
+	protected:
+
+		/**
+		 * The message that is being printed on the output.
+		 * */
+		char* message;
+
+		/**
+		 * Turns the printing of the message on or off.
+		 * */
+		bool state;
 
 	public:
-		HelloWorld();
-		~HelloWorld();
-		virtual void run(void);
-		virtual File<MESSAGE_SIZE>* render( Request* request );
-		virtual Response::status_code process( Request* request, Message** return_message );
 
+		/**
+		 * Class constructor.
+		 * */
+		HelloWorld();
+
+		/**
+		 * Class destructor.
+		 * */
+		~HelloWorld();
+
+		/**
+		 * Executes the resource, effectively printing the message.
+		 * */
+		virtual void run(void);
+
+		/**
+		 * Process a request to this resource.
+		 * @param request the request.
+		 * @param response response
+		 * @return a status code informing on what was done with the request.
+		 * */
+		virtual Response::status_code process( Request* request, Response* response );
 };
 
 #endif /* HELLO_WORLD_H_ */
